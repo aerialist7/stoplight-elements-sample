59a0ca5834f09d688cd381d3c688a13cd8038810
aerialist7 <aerialist7@github.com>
2022-03-31 11:00:01 +0400

    Vite & React & Web Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { API } from '@stoplight/elements'
-
+import '@stoplight/elements/web-components.min.js'
 import '@stoplight/elements/styles.min.css'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"/>
+      {/*@ts-ignore*/}
+      <elements-api
+        apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"
+      />
     </div>
   )
 }
