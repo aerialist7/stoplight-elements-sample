fcc92dd5794467c65f881969d539f145b6449979
aerialist7 <aerialist7@github.com>
2022-03-31 11:23:50 +0400

    Revert "Vite & React & Web Component"
    This reverts commit 59a0ca5834f09d688cd381d3c688a13cd8038810.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import '@stoplight/elements/web-components.min.js'
+import { API } from '@stoplight/elements'
+
 import '@stoplight/elements/styles.min.css'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      {/*@ts-ignore*/}
-      <elements-api
-        apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"
-      />
+      <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"/>
     </div>
   )
 }
